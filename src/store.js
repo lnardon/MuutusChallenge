320a45b5cd9dce2dd8a3b8156144b6192b32d583
Lnardon <lucaslnardon@gmail.com>
2020-07-29 19:13:07 -0300

    Removed Unused Code and Adjusted CSS Finish Button Styles

--- src/store.js
+++ src/store.js
@@ -1,14 +1,8 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import rootReducer from "./reducers";
 
 const INITIAL_STATE = {};
-const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  INITIAL_STATE,
-  applyMiddleware(...middleware)
-);
+const store = createStore(rootReducer, INITIAL_STATE);
 
 export default store;
