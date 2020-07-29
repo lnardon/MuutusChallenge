92ba77d50842438f50c4312c027404a737765df6
Lnardon <lucaslnardon@gmail.com>
2020-07-29 17:55:51 -0300

    Added Text format function to ComicPriceStripe component

--- src/components/ComicPriceStripe/index.js
+++ src/components/ComicPriceStripe/index.js
@@ -3,9 +3,17 @@ import React from "react";
 import "./index.css";
 
 const ComicPriceStripe = ({ type, price }) => {
+  // A little function to transform the camelCase response to Regular English
+  const formatText = (t) => {
+    var text = t;
+    var result = text.replace(/([A-Z])/g, " $1");
+    var finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+    return finalResult;
+  };
+
   return (
     <div className="comicPriceStripeContainer">
-      <h4 className="format">{type}</h4>
+      <h4 className="format">{formatText(type)}</h4>
       <h3 className="price">$ {price}</h3>
     </div>
   );
